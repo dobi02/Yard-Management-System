41b28fd77af460759ac5d86f11afb3d21d136ef2
yj677 <dbswls677@hanyang.ac.kr>
2024-11-17 17:11:54 +0900

    Add dashboard page
    /home/dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/home/dashboard" element={<Dashboard />} />
         </Routes>
     </Router>
   );
