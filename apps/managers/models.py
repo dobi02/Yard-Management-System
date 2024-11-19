272ca90522755699b3db1a511980c822494273be
dongbin <dobi0231@naver.com>
2024-11-19 10:51:41 +0900

    rename manager

--- apps/managers/models.py
+++ apps/managers/models.py
@@ -4,7 +4,8 @@
 #
 from django.db import models
 
-class managers(models.Model):
+
+class Managers(models.Model):
     manager_id = models.CharField(max_length=20, primary_key=True)
     #access_rigths = models.CharField(max_length=10) 어떻게 할지 고민중
     name = models.CharField(max_length=50, null=False)
