79bacfdaf9f84ca6dcc50380f68e3b4bf7429969
yj677 <dbswls677@hanyang.ac.kr>
2024-11-22 14:56:59 +0900

    small fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from "./pages/Admin";
+import Login from "./components/Auth/Login"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
         <Routes>
             <Route path="/admin/*" element={<Main />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     </Router>
   );
