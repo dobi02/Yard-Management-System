0c17ddd735ca405414d5a7afb25b19ce06df7c05
dongbin <dobi0231@naver.com>
2024-12-01 16:52:53 +0900

    feat: add import AvailableSlotsBySiteTypeView

--- apps/places/urls.py
+++ apps/places/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (DivisionsView, DivisionDetailView, YardsView, YardDetailView, SitesView,
-                    SiteDetailView, ParkingSlotDetailView, ParkingSlotsView, AssetCountView)
+                    SiteDetailView, ParkingSlotDetailView, ParkingSlotsView, AssetCountView, AvailableSlotsBySiteTypeView)
 
 urlpatterns = [
     # Divisions
