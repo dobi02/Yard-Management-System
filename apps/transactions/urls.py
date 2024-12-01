acc3959467b0a2838e29361179ce0d5d41b61657
dongbin <dobi0231@naver.com>
2024-12-01 15:31:18 +0900

    feat: change api connect (incomplete)

--- apps/transactions/urls.py
+++ apps/transactions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TransactionsView, TransactionDetailView
 
 urlpatterns = [
-    path('transactions/', TransactionsView.as_view(), name='transactions-list'),
-    path('transactions/<str:pk>/', TransactionDetailView.as_view(), name='transaction-detail'),
+    path('api/transactions/', TransactionsView.as_view(), name='transactions-list'),
+    path('api/transactions/<str:pk>/', TransactionDetailView.as_view(), name='transaction-detail'),
 ]
