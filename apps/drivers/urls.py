01a763e85d40ae13465fc7012489d938fc0057cb
dongbin <dobi0231@naver.com>
2024-12-13 17:38:02 +0900

    feat: driver api problem solved

--- apps/drivers/urls.py
+++ apps/drivers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DriversView, DriverDetailView, DriversYardListView
+from .views import DriversView, DriverDetailView, DriversYardListView, DriversDivisionListView
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/drivers/<str:pk>/', DriverDetailView.as_view(), name='driver-detail'),
     path('api/login', views.login_driver, name='login_driver'),
     path('api/yard-drivers/<str:yard_id>/', views.DriversYardListView.as_view(), name='yard_drivers'),
+    path('api/division-drivers/<str:division_id>/', views.DriversDivisionListView.as_view(), name='division_drivers'),
 ]
