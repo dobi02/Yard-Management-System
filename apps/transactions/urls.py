b5b5d7753330138536904e3fbba5d1d45ca99ee4
blea-ch <66244633+blea-ch@users.noreply.github.com>
2024-12-13 15:50:03 +0900

    trans+driver-choices

--- apps/transactions/urls.py
+++ apps/transactions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TransactionsView, TransactionDetailView, TransactionsByDriverView
+from .views import TransactionsView, TransactionDetailView, TransactionsByDriverView, TransactionsDriverView
 
 urlpatterns = [
     path('api/transactions/', TransactionsView.as_view(), name='transactions-list'),
     path('api/transactions/<str:pk>/', TransactionDetailView.as_view(), name='transaction-detail'),
     path('api/transactions/driver/<str:driver_id>/', TransactionsByDriverView.as_view(), name='transaction-by-driver'),
+    path('api/transactions/driver/<str:driver_id>/<str:pk>/', TransactionsDriverView.as_view(), name='transaction-driver'),
 ]
