4a3d55f8c79f582ca61633cf92410ee576c1c0dc
Uichan Jung <jchan3214@naver.com>
2024-12-14 21:05:50 +0900

    finish listview

--- frontend/src/components/Manager/Dashboard/EquipmentActions.js
+++ frontend/src/components/Manager/Dashboard/EquipmentActions.js
@@ -8,7 +8,7 @@ const EquipmentActions = ({ modals, setModals, isDisabled }) => {
                 type="primary"
                 onClick={() => setModals({ ...modals, add: true })}
                 disabled={isDisabled}
-                style={{ width: '150px' }} // Select와 동일한 크기
+                style={{ width: '150px', marginLeft: '10px' }} // Select와 동일한 크기
             >
                 Add Equipment
             </Button>
@@ -16,7 +16,7 @@ const EquipmentActions = ({ modals, setModals, isDisabled }) => {
                 type="danger"
                 onClick={() => setModals({ ...modals, delete: true })}
                 disabled={isDisabled}
-                style={{ width: '150px' }} // Select와 동일한 크기
+                style={{ width: '150px', marginLeft: '10px' }} // Select와 동일한 크기
             >
                 Delete Equipment
             </Button>
